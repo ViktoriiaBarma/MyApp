82bc92edde35ba6578eb187f3057df14e24a078c
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-12-07 23:29:56 +0200

    updated

--- Screens/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen.jsx
@@ -12,29 +12,41 @@ import Camera from "../assets/svg/camera.svg";
 import Trash from "../assets/svg/trash.svg";
 import MapPin from "../assets/svg/map-pin.svg";
 import Btn from "../Components/Button";
-
 const CreatePostScreen = () => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.wrapper}>
-        <View style={styles.addPhotoWrapper}>
-          <View style={styles.addPhoto}>
-            <Camera />
+        <View>
+          <View style={styles.addPhotoWrapper}>
+            <View style={styles.addPhoto}>
+              <View style={styles.iconAddPhoto}>
+                <Camera />
+              </View>
+            </View>
+            <Text style={styles.addPhotoText}>Завантажте фото</Text>
           </View>
-          <Text>Завантажте фото</Text>
-        </View>
-        <SafeAreaView style={styles.form}>
-          <TextInput placeholder="Назва..." placeholderTextColor="#BDBDBD" />
-          <View>
-            <MapPin />
+          <SafeAreaView style={styles.form}>
             <TextInput
-              placeholder="Місцевість..."
+              style={styles.input}
+              placeholder="Назва"
               placeholderTextColor="#BDBDBD"
             />
-          </View>
+            <View style={{ position: "relative" }}>
+              <MapPin style={styles.icon} />
+              <TextInput
+                style={[styles.input, { paddingLeft: 28 }]}
+                placeholder="Місцевість"
+                placeholderTextColor="#BDBDBD"
+              />
+            </View>
+          </SafeAreaView>
           <Btn>Опубліковати</Btn>
-        </SafeAreaView>
-        <Trash />
+        </View>
+        <View style={styles.deleteIcon}>
+          <View style={styles.deleteIconBlock}>
+            <Trash />
+          </View>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -48,9 +60,64 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "space-between",
+    paddingBottom: 24,
+  },
+  addPhotoWrapper: {
+    marginBottom: 32,
   },
   addPhoto: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     height: 240,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  iconAddPhoto: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+  },
+  addPhotoText: {
+    color: "#BDBDBD",
+    fontSize: 16,
+  },
+  form: {
+    gap: 16,
+    marginBottom: 32,
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 15,
+    borderBottomColor: "#E8E8E8",
+    borderBottomWidth: 1,
+  },
+  icon: {
+    position: "absolute",
+    top: "50%",
+    transform: [{ translateY: -12.5 }],
+  },
+  deleteIcon: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteIconBlock: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F6F6F6",
+    paddingVertical: 10,
+    width: 70,
+    borderRadius: 20,
   },
 });
 
--- Screens/PostsScreen.jsx
+++ Screens/PostsScreen.jsx
@@ -13,33 +13,8 @@ import {
 } from "react-native";
 
 import userImage from "../assets/User.png";
-// import Grid from "../assets/svg/grid.svg";
-// import Post from "../assets/svg/new.svg";
-// import User from "../assets/svg/user.svg";
 
 const PostsScreen = () => {
-  // const [grid, setGrid] = useState(false);
-  // const [post, setPost] = useState(true);
-  // const [user, setUser] = useState(false);
-
-  // const handleGridIsActive = () => {
-  //   setGrid(true);
-  //   setPost(false);
-  //   setUser(false);
-  // };
-
-  // const handlePostIsActive = () => {
-  //   setGrid(false);
-  //   setPost(true);
-  //   setUser(false);
-  // };
-
-  // const handleUserIsActive = () => {
-  //   setGrid(false);
-  //   setPost(false);
-  //   setUser(true);
-  // };
-
   return (
     <View style={styles.container}>
       <View style={styles.body}>
@@ -53,50 +28,14 @@ const PostsScreen = () => {
           </View>
         </View>
       </View>
-      <View style={styles.footer}>
-        {/* <TouchableOpacity
-          style={grid && styles.active}
-          onPress={handleGridIsActive}>
-          <Grid style={grid ? { color: "#fff" } : { color: "#212121" }} />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={post && styles.active}
-          onPress={handlePostIsActive}>
-          <Post style={post ? { color: "#fff" } : { color: "#212121" }} />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={user && styles.active}
-          onPress={handleUserIsActive}>
-          <User style={user ? { color: "#fff" } : { color: "#212121" }} />
-        </TouchableOpacity> */}
-      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  head: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 55,
-    paddingBottom: 10,
+  container: {
+    flex: 1,
     backgroundColor: "#fff",
-    borderBottomColor: "#E5E5E5",
-    borderBottomWidth: 1,
-    position: "relative",
-  },
-  headText: {
-    fontFamily: "Roboto-Medium",
-    color: "#212121",
-    fontSize: 17,
-  },
-  headIcon: {
-    position: "absolute",
-    right: 16,
-    bottom: 10,
   },
   body: {
     paddingVertical: 32,
@@ -121,29 +60,6 @@ const styles = StyleSheet.create({
     color: "#212121",
     opacity: 0.8,
     fontSize: 11,
-  },
-  footer: {
-    borderTopColor: "#E5E5E5",
-    borderTopWidth: 1,
-    paddingTop: 10,
-    paddingBottom: 20,
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-    display: "flex",
-    alignItems: "flex-end",
-    flexDirection: "row",
-    justifyContent: "center",
-    columnGap: 30,
-  },
-  active: {
-    width: 70,
-    backgroundColor: "#FF6C00",
-    color: "#fff",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 20,
   },
 });
 
